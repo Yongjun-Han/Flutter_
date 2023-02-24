636ae883d0c7ae7d02a9d649f5a0581346e5b690
yongjunhan <hanyongjun.dev@gmail.com>
2023-02-24 17:04:35 +0900

    ?? operator

--- function.dart
+++ function.dart
@@ -10,13 +10,31 @@ String sayHello({required String name,required int age ,required String country}
   return 'hello $name, you are $age, and you come from $country!';
 }
 
+//optional positional parameters
+
+String sayBye(String name, int age, [String? country = 'cuba']) => 'Bye $name, $age, $country';
+
+//operator
+
+// String capitalizeName(String? name) => name != null ? name.toUpperCase() : 'ANON';
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANON';
+//?? 는 좌측이 null 이면 우측을 반환
+
 
 void main(){
+  capitalizeName('yongjun');
+  capitalizeName(null);
+
+  String? name ;
+  name ?? 'nico' ;
+
   // print(sayHello("yongjun",28,'ROK'));
 
-  print(sayHello(
-    age : 12,
-    name : 'yongjun',
-    country : 'ROK'
-  ));
+  // print(sayHello(
+  //   age : 12,
+  //   name : 'yongjun',
+  //   country : 'ROK'
+  // ));
+
+  // print(sayBye('yong', 23));
 }
