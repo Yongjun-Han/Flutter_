996cc9dfaef787ee2a85a5146787d34c8925e668
yongjunhan <hanyongjun.dev@gmail.com>
2023-02-24 15:20:45 +0900

    list , collection if

--- main.dart
+++ main.dart
@@ -1,10 +1,15 @@
 void main(){
-  String name = "hello";
-  bool buy = false;
-  int age = 3;
-  double price =  3.4; 
+  // String name = "hello";
+  // bool buy = false;
+  // int age = 3;
+  // double price =  3.4; 
 
-  num x = 13; //int 일수도 double 일수도 잇다  num은 int, double 의 부모 클래스 
+  // num x = 13; //int 일수도 double 일수도 잇다  num은 int, double 의 부모 클래스 
+  var giveMeFive = true;
+
+  var numbers = [1,2,3,4,if(giveMeFive)5,]; //List<int>
+  print(numbers);
   
+  // collection if collection for
 
 }
