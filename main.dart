e0182a572e872d5fee860c9cfeb5a009a4276080
yongjunhan <hanyongjun.dev@gmail.com>
2023-02-24 14:49:57 +0900

    null safety

--- main.dart
+++ main.dart
@@ -1,15 +1,5 @@
-import 'dart:ffi';
-
 void main(){
-  dynamic name; //다이나믹 변수 
-
-  if(name is String) {
-    
-  }
-  if (name is int){
-
-  }
-
-  name = 1;
-  name = true;
+  String? nico = 'nico';
+  nico = null;
+  nico?.isNotEmpty;
 }  
