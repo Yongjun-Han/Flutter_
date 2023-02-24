3d1e21a7c750814a7482cb38f3e966f737c773d0
yongjunhan <hanyongjun.dev@gmail.com>
2023-02-24 15:25:08 +0900

    string interpolation

--- main.dart
+++ main.dart
@@ -5,11 +5,27 @@ void main(){
   // double price =  3.4; 
 
   // num x = 13; //int 일수도 double 일수도 잇다  num은 int, double 의 부모 클래스 
+
+  // collection if
   var giveMeFive = true;
 
   var numbers = [1,2,3,4,if(giveMeFive)5,]; //List<int>
   print(numbers);
   
-  // collection if collection for
+  //string interpolation
+  var name  = '용준';
+  var age = 10;
+
+  var greeting = 'hello everyone my name is $name!. im ${age+2}' ;
+
+  print(greeting);
+
+  //단순변수 는 $,값 변경은 ${}
+
+
+
+
+
+  // collection for
 
 }
