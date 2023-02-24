e4735916bcf1065d06dd74c3e2e24f38392779ad
yongjunhan <hanyongjun.dev@gmail.com>
2023-02-24 16:50:59 +0900

    named Parameter, required

--- function.dart
+++ function.dart
@@ -1,10 +1,22 @@
-String greeting(String potato){
-  return 'hello $potato nice to meet you';
+// String greeting(String potato){
+//   return 'hello $potato nice to meet you';
+// }
+// //void return 값 없음 
+// //arrow function 가능 
+// String greeting2(String potato) => 'hello $potato nice to meet you';
+
+//Namedparameter
+String sayHello({required String name,required int age ,required String country}){
+  return 'hello $name, you are $age, and you come from $country!';
 }
-//void return 값 없음 
-//arrow function 가능 
-String greeting2(String potato) => 'hello $potato nice to meet you';
+
 
 void main(){
-  print(greeting("yongjun"));
+  // print(sayHello("yongjun",28,'ROK'));
+
+  print(sayHello(
+    age : 12,
+    name : 'yongjun',
+    country : 'ROK'
+  ));
 }
