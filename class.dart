45bd49b63dcd6fa1cb61aa4de1eb64a21a44ac72
yongjunhan <hanyongjun.dev@gmail.com>
2023-02-27 15:15:27 +0900

    constructor_02

--- class.dart
+++ class.dart
@@ -2,21 +2,33 @@ class Player {
   //class 는 타입 명시가 필수 
   String name;
   int xp; 
+  String team;
+  int age ;
 
   // Player(String name, int xp){             
   //   this.name = name;
   //   this.xp = xp;
   // }
 
-  Player(this.name, this.xp);
+  Player({
+    required this.name, 
+    required this.xp, 
+    required this.age, 
+    required this.team
+  });
 
   void sayHello(){
-    print('my name is $name');
+    print('my name is $name,$xp,$age,$team');
   }
 }
 
 
 void main(){
-  var player = Player("jason" , 28);
+  var player = Player(
+    name: 'jason',
+    xp: 3242,
+    age: 28,
+    team: 'black'
+  );
   player.sayHello();
 }
